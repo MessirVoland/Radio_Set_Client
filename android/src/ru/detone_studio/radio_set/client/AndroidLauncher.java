4f38610440af2448341e36cbc30bff0d795c9dc8
MessirVoland <messir-voland@mail.ru>
2017-12-04 11:42:40 +0300

    Initial commit

---
+++ android/src/ru/detone_studio/radio_set/client/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package ru.detone_studio.radio_set.client;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import ru.detone_studio.radio_set.client.MainClass;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new MainClass(), config);
+	}
+}
